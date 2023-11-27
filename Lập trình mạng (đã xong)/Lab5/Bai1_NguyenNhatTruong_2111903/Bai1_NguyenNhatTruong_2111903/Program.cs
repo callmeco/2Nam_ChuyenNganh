332ac8027261d42025f17cd18ae3e84b7957a8b5
Nhật Trường <113256750+callmeco@users.noreply.github.com>
2023-11-27 07:58:48 +0700

    Add files via upload

---
+++ Lập trình mạng (đã xong)/Lab5/Bai1_NguyenNhatTruong_2111903/Bai1_NguyenNhatTruong_2111903/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bai1_NguyenNhatTruong_2111903
+{
+    public class MyThreadClass
+    {
+        private const int Random_Sleep_Max = 1000;
+        private const int Loop_Count = 10;
+
+        private string greeting;
+        public MyThreadClass(String greeting)
+        {
+            this.greeting = greeting;
+        }
+
+        public void runMyThread()
+        {
+            Random rand = new Random();
+            for (int x = 0; x < Loop_Count; x++)
+            {
+                Console.WriteLine(greeting + "(Thread ID: " + Thread.CurrentThread.GetHashCode() + ")");
+                try
+                {
+                    Thread.Sleep(rand.Next(0, Random_Sleep_Max));
+                }
+                catch (ThreadInterruptedException)
+                {
+                }
+            }
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThreadClass mtc1 = new MyThreadClass("Tieu trinh thu nhat");
+            Thread thread1 = new Thread(new ThreadStart(mtc1.runMyThread));
+            thread1.Start();
+
+            MyThreadClass mtc2 = new MyThreadClass("Tieu trinh thu hai");
+            Thread thread2 = new Thread(new ThreadStart(mtc2.runMyThread));
+            thread2.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lập trình mạng (đã xong)/Lab5/Bai2_NguyenNhatTruong_2111903/Bai2_NguyenNhatTruong_2111903/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_NguyenNhatTruong_2111903
+{
+    public interface ILogger
+    {
+        void writeEntry(ArrayList entry);
+        void writeEntry(String entry);
+    }
+}
